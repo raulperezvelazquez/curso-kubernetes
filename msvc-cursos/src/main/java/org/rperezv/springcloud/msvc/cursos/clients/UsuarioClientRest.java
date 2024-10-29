5ded718598396df3d3b0f62870c4023565c3ec85
Jimena Hernando <jimenahernando@hotmail.com>
2024-10-29 16:16:03 +0100

    Dockerizado de Curso

--- msvc-cursos/src/main/java/org/rperezv/springcloud/msvc/cursos/clients/UsuarioClientRest.java
+++ msvc-cursos/src/main/java/org/rperezv/springcloud/msvc/cursos/clients/UsuarioClientRest.java
@@ -1,13 +1,12 @@
 package org.rperezv.springcloud.msvc.cursos.clients;
 
-import org.bouncycastle.util.Iterable;
 import org.rperezv.springcloud.msvc.cursos.models.Usuario;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@FeignClient(name="msvc-usuarios", url="localhost:8001")
+@FeignClient(name="msvc-usuarios", url="msvc-usuarios:8001")
 public interface UsuarioClientRest {
 
     @GetMapping("/{id}")
